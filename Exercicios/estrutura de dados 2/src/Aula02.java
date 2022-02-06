9470131a21e8e419cba84bdd7b6b490f0f02673b
[Sabrina P Ramos] <[sparamos@gmail.com]>
2022-02-06 16:40:49 -0300

    Base da aula 2.

---
+++ Exercicios/estrutura de dados 2/src/Aula02.java
@@ -0,0 +1,11 @@
+public class Aula02 {
+
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i =0;
+        i++;
+
+    }
+
+}
---
+++ Exercicios/estrutura de dados 2/src/Vetor.java
@@ -0,0 +1,9 @@
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+
+}
